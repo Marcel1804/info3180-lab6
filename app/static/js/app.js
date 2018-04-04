9c20b671f463a5f11ce135bf6632faa03148b88d
Akeam Williams <akeam.williams@mymona.uwi.edu>
2018-04-04 17:37:15 +0000

    fixing layout

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -41,27 +41,30 @@ Vue.component('app-footer', {
 
 const NewsList=Vue.component('news-list',{
     template:`
-           <div class="news">
-                <h2 class=center>News</h2>
-               
-                    <li v-for="article in articles"class="news__list">
-                     <h4>{{article.title}}</h4>
-                     <img v-bind:src="article.urlToImage" alt="news image" style="width:360px;height:300px;"/>
-                     <br>
-                     {{article.description}} 
-                    </li>
-               
-            </div>
+            <div>
             <div class="form-inline d-flex justify-content-center">
                  <div class="form-group mx-sm-3 mb-2">
                  <label class="sr-only" for="search">Search</label>
                  <input type="search" name="search" v-model="searchTerm"
-                id="search" class="form-control mb-2 mr-sm-2" placeholder="Enter
-                search term here" />
+                id="search" class="form-control mb-2 mr-sm-2" placeholder="Enter search term here" />
                 <button class="btn btn-primary mb-2"@click="searchNews">Search</button>
                  </div>
             </div>
-                
+            <div class="news">
+                <h2 class=center>News</h2>
+                 <ul class="news__list">
+                    <li v-for="article in articles"class="news__item" >
+                     <div class="new_item">
+                     <h6>{{article.title}}</h6>
+                     <img v-bind:src="article.urlToImage" alt="news image" style="width:200px;height:200px;"/>
+                     <br>
+                     {{article.description}} 
+                     </div>
+                    </li>
+                <ul>
+               
+            </div>
+            </div>
           `,
     created: function(){
         let self =this;
